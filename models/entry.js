707aa69caac89c820a585452546530f150b36775
boyboi86 <boyboi86@gmail.com>
2016-09-15 23:57:02 +0800

    include q-promise pluggin

--- models/entry.js
+++ models/entry.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
-const Schema = mongoose.Schema
+const { Schema } = mongoose
 
 const entrySchema = new Schema({
   title: {
