0068a745b0f29dd4b0d2a48ac00f278c53f5d7a6
boyboi86 <boyboi86@gmail.com>
2016-09-20 00:03:25 +0800

    update user route with SignIn & SignUp

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const Auth = require('../controllers/authenticate');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/*Sign In Route */
+router.post('/signin', Auth.SignIn);
+
+/*Sign Up Route */
+router.post('/signup', Auth.SignUp);
 
 module.exports = router;
