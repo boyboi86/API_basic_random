29c845e9615f76a96039d0e1115a3e6cbf416079
boyboi86 <boyboi86@gmail.com>
2016-09-22 22:02:05 +0800

    created Salt/hash for each sign up

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
-const { Schema } = mongoose
+const { Schema } = mongoose;
+const Q = require('q');
 const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new Schema({
@@ -12,6 +13,20 @@ const userSchema = new Schema({
   password: String
 }, { timestamps: true })
 
+/* Async salt/hash for each signup before saving user details */
+userSchema.pre('save', function(next){
+  let user = this;
+  bcrypt.genSalt(10,function(err, salt){
+    if(err){ next(err) }
+    bcrypt.hash(user.password, salt, null, function(err, hash_password){
+      if(err){ next(err) }
+        user.password = hash_password
+        next();
+    })
+  })
+})
+
 const UserModel = mongoose.model('User', userSchema);
+
 
 module.exports = UserModel;
