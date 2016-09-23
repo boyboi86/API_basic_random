0a858090e54c9fcf2ad53116851c4f5243ff4aaa
boyboi86 <boyboi86@gmail.com>
2016-09-24 03:21:11 +0800

    promised-base strategies

--- services/passport.js
+++ services/passport.js
@@ -10,27 +10,46 @@ const localOptions = { usernameField: 'email' }
 const localAuth = new LocalStrategy( localOptions, function(email, password, done){
   User.findOne({ email: email })
   .then(function(user){
-    if(!user){
-      done(null, false);
-    }
-     user.comparePassword(password, function(isMatch){
-       isMatch? done(null, isMatch): done(null, false)
-     })
-  })
+    return new Promise(function(resolve, reject){
+      if(!user){
+        console.error('invalid user auth')
+        return done(null, false)
+      }
+      user.comparePassword(password, function(err, isMatch){
+          if(err){
+              console.error('error while comparing')
+              return done(err)
+            }
+          if(!isMatch){
+             console.error('not matching password')
+             return done(null, false)
+           }
+           console.log('local strategy resolved');
+           return resolve(done(null, isMatch))
+        })
+      })
+    })
   .catch(function(err){
     return done(err)
   })
 })
 
 const JwtOptions = {
-  jwtFromRequest: ExtractJwt.fromHeader('Authorization'),
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: config.secret
 };
 
 const JwtAuth = new JwtStrategy( JwtOptions, function(payload, done){
   User.findById( payload.sub )
   .then(function(user){
-    user ?  done(null, user) :  done(null, false)
+    return new Promise(function(resolve, reject){
+      if(!user){
+        console.error('token does not match')
+        return done(null, false)
+      }
+      console.log('JWT Strategy resolve')
+      return resolve(done(null, user))
+    })
   })
   .catch(function(err){
     return done(err)
