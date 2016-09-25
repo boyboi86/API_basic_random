d5433fd0e81e169e5ea26d45363f45420de31a73
boyboi86 <boyboi86@gmail.com>
2016-09-25 22:14:50 +0800

    added webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack =  require('webpack');
+const path = require('path');
+const debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  entry: ['./public/src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './public'
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ]
+};
