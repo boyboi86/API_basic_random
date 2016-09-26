e35db9ebb4e2640e06899efd34ea9ab48b609646
boyboi86 <boyboi86@gmail.com>
2016-09-26 18:00:50 +0800

    added indexpage

---
+++ public/src/components/indexpage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Indexpage  extends Component {
+  render(){
+    return (
+      <div>random page for server redirects</div>
+    )
+  }
+}
