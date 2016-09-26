ee6b7eba663f05414045830ef32508a90d061074
boyboi86 <boyboi86@gmail.com>
2016-09-27 00:29:21 +0800

    added React-Router Links

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,6 @@ const User = require('../models/user');
 /* GET home page. if there is valid jwt(user didn't log out) redirect to entries if not redirect to users route*/
 router.get('/', function(req, res, next) {
   res.send('welcome!!');
-  const auth = req.header.authorization
-  if(!auth){
-    res.redirect('/users')
-  }
-  const _id = jwt.verify(auth, config.secret)
-  User.findOne({ _id }).exec(function(err, validUser){
-    if(err || !validUser) {
-      res.redirect('/users')
-    }
-    res.redirect('/entries')
-  })
 });
 
 module.exports = router;
