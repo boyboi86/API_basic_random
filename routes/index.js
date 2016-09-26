4550944e20e3f9a1bbc67c47dd9eb723e827467e
boyboi86 <boyboi86@gmail.com>
2016-09-26 13:21:31 +0800

    update redirect for index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const User = require('../models/user');
+
+/* GET home page. if there is valid jwt redirect to entries if not redirect to users route*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'React/Redux Blog' });
+  res.send('welcome!!');
+  const auth = req.header.authorization
+  if(!auth){
+    res.redirect('/users')
+  }
+  const _id = jwt.verify(auth, config.secret)
+  User.findOne({ _id }).exec(function(err, validUser){
+    if(err || !validUser) {
+      res.redirect('/users')
+    }
+    res.redirect('/entries')
+  })
 });
 
 module.exports = router;
