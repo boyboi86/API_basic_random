4631164041c1141eaa04bc2daa2327d949ea4528
boyboi86 <boyboi86@gmail.com>
2016-09-28 16:19:13 +0800

    added links for easier navigation

--- public/src/components/indexpage.js
+++ public/src/components/indexpage.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
-export default class Indexpage  extends Component {
+export default class Indexpage extends Component {
   render(){
     return (
-      <div>random page for server redirects</div>
+      <div>
+        <div>This is build with React/Redux</div>
+        <Link className="btn btn-primary" to="/Entry">Profile</Link>
+        <Link className="btn btn-info" to="/users">See all users</Link>
+      </div>
     )
   }
 }
