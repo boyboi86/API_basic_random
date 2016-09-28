7376be7d6a9f54d4c4258156ac5c17e40e3d4127
boyboi86 <boyboi86@gmail.com>
2016-09-28 16:18:03 +0800

    include types for GET/POST/DELETE/UPDATE posts

--- public/src/actions/types.js
+++ public/src/actions/types.js
@@ -1,3 +1,7 @@
 export const AUTH_USER = 'auth_user';
 export const UNAUTH_USER = 'unauth_user';
 export const AUTH_ERROR = 'auth_error';
+export const GET_USERS = 'get_users';
+export const GET_POSTS = 'get_posts';
+export const DELETE_POST = 'delete_post';
+export const POST_ENTRY = 'post_entry';
