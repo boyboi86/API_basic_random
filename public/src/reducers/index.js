d2e43a7cb04a6fc6b879fe74d0a3ac866cd3ac75
boyboi86 <boyboi86@gmail.com>
2016-09-28 16:16:23 +0800

    added combined reducer for user/posts/auth

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import auth from './auth_reducer';
+import users from './user_reducer';
+import posts from './post_reducer';
 
 const rootReducer = combineReducers({
   form,
-  auth
+  auth,
+  users,
+  posts
 });
 
 export default rootReducer;
