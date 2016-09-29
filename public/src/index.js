1abaa355ebdbd49c4c439e1c62402a395311b741
boyboi86 <boyboi86@gmail.com>
2016-09-29 14:54:18 +0800

    error handling for routing and update params for nested route

--- public/src/index.js
+++ public/src/index.js
@@ -8,14 +8,15 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
 import Indexpage from './components/indexpage';
-import Users from './components/users';
+import Users from './components/entries/users';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
 import Signout from './components/auth/signout';
-import Entry from './components/entry';
-import NewEntry from './components/newentry';
-import EditEntry from './components/editentry';
+import Entry from './components/entries/entry';
+import NewEntry from './components/entries/new_entry';
+import EditEntry from './components/entries/edit_entry';
 import requiredAuth from './components/auth/require_auth';
+import NotFound from './components/notfound';
 
 import { AUTH_USER } from './actions/types';
 import reducers from './reducers';
@@ -39,8 +40,9 @@ ReactDOM.render(
         <Route path="signup" component={ Signup } />
         <Route path="users" component= { requiredAuth(Users) } />
         <Route path="entry/new" component= { requiredAuth(NewEntry) }  />
-        <Route path="entry/edit" component= { requiredAuth(EditEntry) }  />
-        <Route path="entry" components= { requiredAuth(Entry) } />
+        <Route path="entry/edit/:id" component= { requiredAuth(EditEntry) }  />
+        <Route path="entry" component= { requiredAuth(Entry) } />
+        <Route path="*" component = { NotFound } />
       </Route>
     </Router>
   </Provider>
