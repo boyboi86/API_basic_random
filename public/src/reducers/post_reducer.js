d64993d02f54429da22a5461f2522986087fd30a
boyboi86 <boyboi86@gmail.com>
2016-09-29 14:53:31 +0800

    update sub-reducer for entries patch actions

--- public/src/reducers/post_reducer.js
+++ public/src/reducers/post_reducer.js
@@ -1,9 +1,24 @@
 import { GET_POSTS } from '../actions/types';
+import { GET_EDITPOST } from '../actions/types';
+import { PATCH_POST } from '../actions/types';
+import { DELETE_POST } from '../actions/types';
+import { PATCH_EDITPOST } from '../actions/types';
+import { PATCH_EDITPOST_DESC } from '../actions/types';
 
 export default function( state = { post: [] }, action){
   switch (action.type) {
     case GET_POSTS:
-      return { ...state , post: action.payload.data };
+      return { ...state, post: action.payload.data };
+    case GET_EDITPOST:
+      return { ...state, post: action.payload.data };
+    case PATCH_POST:
+      return { ...state, post: action.payload.data };
+    case DELETE_POST:
+      return { ...state, post: action.payload.data };
+    case PATCH_EDITPOST:
+      return {...state, post: action.payload };
+    case PATCH_EDITPOST_DESC:
+      return {...state, post: action.payload };
     }
     return state;
   }
