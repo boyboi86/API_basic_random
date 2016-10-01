1c8307666257f30b3df7e0b1cf004530132407b2
boyboi86 <boyboi86@gmail.com>
2016-10-01 13:06:08 +0800

    refactor routes to render public folder after server routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 /* GET home page public static files*/
-router.get('/', function(req, res, next) {
-  res.json({message: 'welcome!!'});
 
+router.get('*', function(req, res){
+  res.sendFile(path.resolve(__dirname, '../public/index.html'))
 });
-
-// router.get('/*', function(req, res){
-//   res.sendFile(path.resolve(__dirname, 'index.html'))
-// });
 
 module.exports = router;
