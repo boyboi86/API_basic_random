55b2000db244b55b6ffb53b44042a85b9ed589f8
Winson <boyboi86@users.noreply.github.com>
2016-10-01 15:59:46 +0800

    Update ROOT_URL

--- public/src/actions/index.js
+++ public/src/actions/index.js
@@ -13,7 +13,7 @@ import {
   PATCH_EDITPOST,
   PATCH_EDITPOST_DESC } from './types';
 
-const ROOT_URL = "https://lit-savannah-16561.herokuapp.com"
+const ROOT_URL = "http://localhost:3000"
 const axiosOption = {headers: { authorization : localStorage.getItem('token')}}
 
 /*Sign in user*/
