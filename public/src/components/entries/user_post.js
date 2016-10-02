dd7eb82d4bc794c9273d2e5260675ad2d3e82b0d
boyboi86 <boyboi86@gmail.com>
2016-10-02 14:52:40 +0800

    added new page for a list of other user's post

---
+++ public/src/components/entries/user_post.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as action from '../../actions';
+import moment from 'moment';
+
+class UsersPost extends Component {
+
+  componentWillMount(){
+    const {id} = this.props.params
+    this.props.getOtherUsersPost({ id })
+  }
+
+  getTimeFrom(time){
+    const updatedAt = new Date(time);
+    return moment(updatedAt, 'YYYYMMDD').fromNow()
+  }
+
+userPostHandle(){
+  if(!this.props.users.entries || !this.props.users.entries[0]){
+    return (
+      <div><strong>Loading..!!</strong></div>
+    )
+  }
+  return this.props.users.entries.map(function(el, index){
+    return(
+      <li className="list-group-item" key={index}>
+        <div>
+          <b>{ el.title }</b>
+          <i className="pull-sm-right">{this.getTimeFrom(el.updatedAt)}</i>
+          <div>{el.description}</div>
+        </div>
+      </li>
+    )
+  },this)
+}
+
+  render(){
+    return (
+      <div className="jumbotron container">
+        <ul className="list-group col-sm-6 offset-sm-3">
+        {this.userPostHandle()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state){
+  return {
+    users: state.users.user
+  }
+}
+
+export default connect(mapStateToProps, action)(UsersPost)
