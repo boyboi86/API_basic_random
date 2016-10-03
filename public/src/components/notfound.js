7d5589e46c7bd9c028d45992ab6936d631079d0c
boyboi86 <boyboi86@gmail.com>
2016-10-03 14:51:05 +0800

    update layout for 404

--- public/src/components/notfound.js
+++ public/src/components/notfound.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export default class NotFound extends Component{
   render(){
     return(
-      <div><strong>It seems like you are lost!!</strong></div>
+      <div className="col-sm-6 offset-sm-3">
+        <h1 className="center"><strong>404</strong></h1>
+        <br />
+        <strong>It seems like you are lost!!</strong>
+      </div>
     )
   }
 }
