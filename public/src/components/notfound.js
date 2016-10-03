9976a3c99ce4e09473686fe2a91f8571d341b98f
boyboi86 <boyboi86@gmail.com>
2016-10-04 00:19:21 +0800

    add additional styling for 404

--- public/src/components/notfound.js
+++ public/src/components/notfound.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class NotFound extends Component{
   render(){
     return(
-      <div className="col-sm-6 offset-sm-3">
+      <div className="col-sm-6 offset-sm-3 jumbotron">
         <h1 className="center"><strong>404</strong></h1>
         <br />
         <strong>It seems like you are lost!!</strong>
